88bf72dd413c42e1c25afbfca8906325caa03c74
Jon Cooper <joncoop@users.noreply.github.com>
2017-09-07 07:34:06 -0400

    Add files via upload

---
+++ guess_v1.py
@@ -0,0 +1,19 @@
+import random
+
+rand = random.randrange(1,100)
+print("I'm thinking of a number from 1 to 100.");
+
+guess = -1
+
+while guess != rand:
+    guess = input("Take a guess: ")
+    guess = int(guess)
+    
+    if guess < rand:
+        print("You guessed too low.")
+    elif guess > rand:
+        print("You guessed too high.")
+    else:
+        print("You got it!")
+
+print("Game over")
