a9d0b98a40882433226ff724acecbc9719c835a7
Cooper <jccooper@greenville.k12.sc.us>
2017-09-13 09:22:59 -0400

    Added high/low config

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,7 +1,11 @@
 import random
 
-rand = random.randint(1, 100)
-print("I'm thinking of a number from 1 to 100.");
+# config
+low = 1
+high = 100
+
+rand = random.randint(low, high)
+print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
 guess = -1
 
